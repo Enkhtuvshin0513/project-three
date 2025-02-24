fc882656f78d9cc28ac2298aaf340022294e26a8
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-02-24 19:32:31 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("1231");
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+mongoose.connect().then(() => {
+  console.log("connected to MONGO");
+});
+
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.send("succes");
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
