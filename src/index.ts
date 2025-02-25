37c6d66f825b48b4d98c1003083e977207799971
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-02-25 17:49:16 +0800

    add typescript class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
-mongoose.connect().then(() => {
+mongoose.connect("12").then(() => {
   console.log("connected to MONGO");
 });
 
